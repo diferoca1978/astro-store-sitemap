44e4c12d3363a6f1676b6d6a875b8eed79f9d558
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2024-12-09 18:50:43 -0500

    without bring the correct info of the user role

--- auth.config.ts
+++ auth.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'auth-astro';
 import Credentials from '@auth/core/providers/credentials';
 import { db, eq, ne, user } from 'astro:db';
+import type { AdapterUser } from '@auth/core/adapters';
 
 
 export default defineConfig({
@@ -20,17 +21,47 @@ export default defineConfig({
 
       authorize: async ({ email, password }) => {
 
-        const client = await db.select().from(user).where(eq(user.email, `${email}`));
+        const [client] = await db.select().from(user).where(eq(user.email, `${email}`));
 
+        console.log("🚀 ~ authorize: ~ client:", client)
         if (!client) {
           throw new Error('Invalid credentials')
         }
 
-        return {
-          id: 'abcd',
-          name: 'anything'
-        }
+        // TODO: When the insert an user into our database, make the method to validate the password.
+
+        const { password: _, ...rest } = client
+        console.log("🚀rest:", rest)
+        return { ...rest, id: rest.id.toString(), role: rest.role.toString() } // Here we're transform the id that is type number to a string to it doesn't crash with the type of authorize method.
+
       }
     })
   ],
+
+  // Here we'll define the callbacks to expand our object session information.
+
+  callbacks: {
+
+    jwt: ({ token, user }) => {
+
+      if (user) {
+        token.user = user
+      }
+
+      return token
+    },
+
+    session: ({ session, token }) => {
+
+
+      session.user = token.user as AdapterUser;
+
+      console.log({ UserSession: session.user });
+
+      return session
+    }
+  },
+
+
+
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,24 @@
 import { defineMiddleware } from "astro:middleware"
+import { getSession } from 'auth-astro/server';
 
 const noAuthRoutes = ['/login', '/register']
 
-export const onRequest = defineMiddleware(async ({ url, locals, redirect }, next) => {
+export const onRequest = defineMiddleware(async ({ url, locals, redirect, request }, next) => {
 
-  const isLoggedIn = false
+  const session = await getSession(request)
+  const isLoggedIn = !!session // The double negation means that have a session.
+  const user = session?.user // Here we take the session object to do the things easier
 
   locals.isLoggedIn = isLoggedIn,
     locals.user = null
 
-  if (locals.user) {
+  if (user) { // Here we check if there is an user
+    locals.user = {
+      name: user.name!, // With the exclamation character we say that always have a email an password 
+      email: user.email!
+    }
 
+    locals.isAdmin = user. === 'admin'
   }
 
   // Then have the control the access through the user role.
