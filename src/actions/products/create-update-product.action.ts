bed084bde3fad8691bddc762b95cf5ba677e0f21
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2025-01-09 17:10:18 -0500

    load images from server action in the backend.

--- src/actions/products/create-update-product.action.ts
+++ src/actions/products/create-update-product.action.ts
@@ -22,6 +22,20 @@ import { v4 as uuidv4 } from 'uuid'
  * @throws {Error} If the user is not authenticated.
  * 
  */
+
+
+//Protip: in JavaScript we can set the separation of thousands by underscore.
+const MAX_FILE_SIZE = 5 * 1024 * 1024 // equal 5MB
+const ACCEPTED_IMAGE_TYPES = [
+  'image/jpg',
+  'image/jpeg',
+  'image/png',
+  'image/webp',
+  'image/svg+xml',
+  'image/gif',
+];
+
+
 export const createUpdateProduct = defineAction({
   accept: 'form',
   input: z.object({
@@ -34,7 +48,17 @@ export const createUpdateProduct = defineAction({
     stock: z.number(),
     tags: z.string(),
     title: z.string(),
-    type: z.string()
+    type: z.string(),
+
+    imageFiles: z.array(
+      z.instanceof(File)
+        .refine(file => file.size <= MAX_FILE_SIZE, 'Max image size accepted 5MB')
+        .refine(file => {
+          if (file.size === 0) return true;
+
+          return ACCEPTED_IMAGE_TYPES.includes(file.type);
+        }, `Only suported image files are valid`)
+    ).optional()
   }),
   handler: async (form, { request }) => {
 
@@ -45,14 +69,14 @@ export const createUpdateProduct = defineAction({
       throw new Error('Unauthorized')
     }
 
-    const { id = uuidv4(), ...rest } = form;
+    const { id = uuidv4(), imageFiles, ...rest } = form;
     rest.slug = rest.slug.toLowerCase().replaceAll(' ', '-').trim();
 
     const productToCreate = {
       id: id,
       user: user.id!,
       ...rest
-    }
+    };
 
     //create
     if (!form.id) {
@@ -64,7 +88,8 @@ export const createUpdateProduct = defineAction({
 
     //insert images
 
-    console.log({ productToCreate });
+    console.log({ imageFiles });
+
 
 
     return productToCreate;
